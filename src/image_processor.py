c5319d47e6d8b33c893dbdae0b66fd8a9d2f37e7
Jonh Ferreira <jonhifs@gmail.com>
2022-05-25 16:51:13 -0300

    feat: modificação na forma de identificar bordas de poligonos

--- src/image_processor.py
+++ src/image_processor.py
@@ -3,168 +3,219 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-class ImageProcessor:
-    # Função para limpar comentários
-    def clearComment(matrix_img):
-        cont = 0
-        comments = []
-        for line in matrix_img:
-            if '#' in line:
-                cont = 0
-                comments.append(line)
 
-            elif cont >= 4:
-                break
+# Função para limpar comentários
+def clearComment(matrix_img):
+    cont = 0
+    comments = []
+    for line in matrix_img:
+        if '#' in line:
+            cont = 0
+            comments.append(line)
 
-            else:
-                cont += 1
+        elif cont >= 4:
+            break
 
-        for comment in comments:
-            matrix_img.remove(comment)
+        else:
+            cont += 1
 
-        return matrix_img
+    for comment in comments:
+        matrix_img.remove(comment)
 
-    # Função para obtenção de imagens em  uma matriz
-    def getImageP1(name_img):
-        file_img = open(name_img, 'r')
-        matrix_img = file_img.readlines()
+    return matrix_img
 
-        matrix_img = clearComment(matrix_img)
+# Função para obtenção de imagens em  uma matriz
+def getImageP1(name_img):
+    file_img = open(name_img, 'r')
+    matrix_img = file_img.readlines()
 
-        # Tipo de arquivo
-        type_img = matrix_img[0][0:2]
+    matrix_img = clearComment(matrix_img)
 
-        # Obtenção da largura e altura da imagem
-        line_size = matrix_img[1].replace('\n', '').split()
-        larg, alt = int(line_size[0]), int(line_size[1])
+    # Tipo de arquivo
+    type_img = matrix_img[0][0:2]
 
-        # Obtenção da matriz da imagem pura, sem cabeçalho e em uma lista
-        ini = 2
-        image = []
-        for i in range(alt):
-            line = []
-            for j in range(ini, ini + larg):
-                line.append(int(matrix_img[j]))
-            image.append(line)
-            ini += larg
+    # Obtenção da largura e altura da imagem
+    line_size = matrix_img[1].replace('\n', '').split()
+    larg, alt = int(line_size[0]), int(line_size[1])
 
-        image_np = np.array(image)
-        file_img.close()
+    # Obtenção da matriz da imagem pura, sem cabeçalho e em uma lista
+    ini = 2
+    image = []
+    for i in range(alt):
+        line = []
+        for j in range(ini, ini + larg):
+            line.append(int(matrix_img[j]))
+        image.append(line)
+        ini += larg
 
-        return (type_img, larg, alt, image_np)
+    image_np = np.array(image)
+    file_img.close()
 
-    # Função que salva uma matriz como imagem
-    def saveImage(type_img, larg, alt, image):
-        name_img = "imagem" + str(random.randint(0, 10000)) + ".pgm"
-        type_img = type_img + "\n"
-        size = str(larg) + " " + str(alt) + "\n"
-        header = [type_img, size]
+    return (type_img, larg, alt, image_np)
 
-        file_img = open('result/'+name_img, 'w')
+# Função que salva uma matriz como imagem
+def saveImage(type_img, larg, alt, image):
+    name_img = "imagem" + str(random.randint(0, 10000)) + ".pgm"
+    type_img = type_img + "\n"
+    size = str(larg) + " " + str(alt) + "\n"
+    header = [type_img, size]
 
-        # Escreve o cabeçalho
-        for content in header:
-            file_img.write(content)
+    file_img = open('../results/'+name_img, 'w')
 
-        for i in range(alt):
-            for j in range(larg):
-                pixel = str(image[i][j]) + '\n'
-                file_img.write(pixel)
+    # Escreve o cabeçalho
+    for content in header:
+        file_img.write(content)
 
-        file_img.close()
+    for i in range(alt):
+        for j in range(larg):
+            pixel = str(image[i][j]) + '\n'
+            file_img.write(pixel)
 
-    # Função que dilata a imagem
-    def dilateEffect(image, larg, alt, elem_struct):
-        # Informações do elemento estruturante
-        larg_elem = len(elem_struct[0])
-        alt_elem = len(elem_struct)
-        center = int(alt_elem / 2)
+    file_img.close()
 
-        img_res = image.copy()
+# Função que dilata a imagem
+def dilateEffect(image, larg, alt, elem_struct):
+    # Informações do elemento estruturante
+    larg_elem = len(elem_struct[0])
+    alt_elem = len(elem_struct)
+    center = int(alt_elem / 2)
 
-        for i in range(1, alt-center):
+    img_res = image.copy()
 
-            for j in range(1, larg-center):
-                ver = False
+    for i in range(1, alt-center):
 
-                for ie in range(alt_elem):
+        for j in range(1, larg-center):
+            ver = False
 
-                    for je in range(larg_elem):
-                        n_i = (i-1) + ie
-                        n_j = (j-1) + je
+            for ie in range(alt_elem):
 
-                        if (elem_struct[ie][je] != 0) and (image[n_i][n_j] == elem_struct[ie][je]):
-                            img_res[i - 1 + center][j - 1 + center] = elem_struct[center][center]
-                            ver = True
-                            break
+                for je in range(larg_elem):
+                    n_i = (i-1) + ie
+                    n_j = (j-1) + je
 
-                    if ver:
+                    if (elem_struct[ie][je] != 0) and (image[n_i][n_j] == elem_struct[ie][je]):
+                        img_res[i - 1 + center][j - 1 + center] = elem_struct[center][center]
+                        ver = True
                         break
 
-        return img_res
+                if ver:
+                    break
 
-    # Realiza a operação de subtração entre duas imagens
-    def subtractionEffect(image1, image2):
-        img_res = abs(image1 - image2)
-        return img_res
+    return img_res
 
-    # Identifica as bordas dos objetos na imagem
-    def identifyBorder(name_img):
-        type_img, larg, alt, image = getImageP1(name_img)
+# Realiza a operação de subtração entre duas imagens
+def subtractionEffect(image1, image2):
+    img_res = abs(image1 - image2)
+    return img_res
 
-        image_copy = image.copy()
-        elem_struct = np.array([[0, 1, 0], [0, 1, 0], [0, 0, 0]])
+# Identifica as bordas dos objetos na imagem
+def identifyBorder(name_img):
+    type_img, larg, alt, image = getImageP1(name_img)
 
-        image_dilated = dilateEffect(image_copy, larg, alt, elem_struct)
-        image_sub = subtractionEffect(image_dilated, image_copy)
+    image_copy = image.copy()
+    elem_struct = np.array([[0, 1, 0], [0, 1, 0], [0, 0, 0]])
 
-        saveImage(type_img, larg, alt, image_sub)
+    image_dilated = dilateEffect(image_copy, larg, alt, elem_struct)
+    image_sub = subtractionEffect(image_dilated, image_copy)
 
-    # Identifica a quantidade de polígonos presentes na imagem
-    def identifyPolygon(img_name):
-        tipo_img, larg, alt, image = getImageP1(img_name)
+    saveImage(type_img, larg, alt, image_sub)
 
-        # Percorre as coordenadas salvando os pixels pretos formando as linhas
-        coord = []
-        for i in range(alt):
+# Identifica a quantidade de polígonos presentes na imagem
+def identifyPolygon(img_name):
+    tipo_img, larg, alt, image = getImageP1(img_name)
+    
 
-            for j in range(larg):
+    # Percorre as coordenadas salvando os pixels pretos formando as linhas
+    coord = []
+    for i in range(alt):
 
-                if image[i][j] == 1:
-                    coord.append([i, j])
+        for j in range(larg):
 
-        # Ordena as coordenadas
-        coord.sort()
+            if image[i][j] == 1:
+                coord.append([i, j])
 
-        # Matriz de retas
-        lines = [[]]
-        num_lines = 0
+    # Ordena as coordenadas
+    coord.sort()
+    coord_copy = coord.copy()
+    # Matriz de retas
+    lines = [[]]
+    num_lines = 0
+       
+    while len(coord_copy) != 0:
+        i, j = coord_copy[0]
 
-        for c in range(len(coord)):
-            i, j = coord[c]
-            # Verifica uma posição afrente, diagonal inferiro direita, diagonal inferior esquerda, diagonal superior esquerda e diagonal superior direita, respctivamente
-            if (([i, j + 1] in coord) or ([i + 1, j + 1] in coord) or ([i + 1, j - 1] in coord) or ([i - 1, j - 1] in coord) or ([i - 1, j + 1] in coord)):
+        veri = 10
+        ant_ver = 10 
+
+        while veri != 0:
+                
+            if ([i, j + 1] in coord_copy):
                 lines[num_lines].append([i, j])
+                coord_copy.remove([i,j])
+                j = j+1
+                veri += 1
+                    
+            if ([i + 1, j + 1] in coord_copy):
+                lines[num_lines].append([i, j])
+                coord_copy.remove([i,j])
+                i = i+1
+                j = j+1
+                veri += 1
+
+            if ([i+1,j] in coord_copy):
+                lines[num_lines].append([i, j])
+                coord_copy.remove([i,j])
+                i = i+1
+                veri += 1
+
+            if ([i-1, j] in coord_copy):
+                lines[num_lines].append([i, j])
+                coord_copy.remove([i,j])
+                i = i-1
+                veri += 1
+
+            if ([i - 1, j + 1] in coord_copy):
+                lines[num_lines].append([i, j])
+                coord_copy.remove([i,j])
+                i = i-1
+                j = j+1
+                veri += 1
+
+            veri = veri - ant_ver 
+            ant_ver = veri
+
+        lines[num_lines].append([i, j])
+        coord_copy.remove([i,j])
+
+        lines.append([])
+        num_lines += 1
+
+    # Contagem de polígonos
+    count = 0
+
+    # Lista de polígonos
+    polygons = []
+
+    # Coordenadas
+    cord_retas = []
+    
+    for line in lines:
+        if len(line) > 10:
+            polygons.append(line)
+            cor = [[], []]
             
-            else:
-                lines[num_lines].append([i, j])
-                lines.append([])
-                num_lines += 1
+            for ponto in line:
+                cor[0].append(648-ponto[0])
+                cor[1].append(ponto[1])
+            cord_retas.append(cor)
+            count += 1
 
-        # Contagem de polígonos
-        count = 0
+    for y, x in cord_retas:
+        plt.plot(x,y)
 
-        # Lista de polígonos
-        polygons = []
+    plt.savefig(img_name[:-4]+"_grafico"+".png",dpi=300)
 
-        # Coordenadas
-        x = []
-        y = []
-        for line in lines:
-            if len(line) > 10:
-                polygons.append(line)
-                y.append(line[0])
-                x.append(line[1])
-                count += 1
+    plt.close()
 
-        print(count)
+    print(count)
+    
