62534dcab42409d6cece84c3ebc3fed491abeb2d
Ayrton Hora <ayrtonfh@gmail.com>
2022-05-28 15:23:11 -0300

    style: Documentação e Testes finais

--- src/image_processor.py
+++ src/image_processor.py
@@ -2,103 +2,108 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import time
 import os
 
 # Função para limpar comentários
-def clearComment(matrix_img):
-    cont = 0
+def clearComment(img_matrix):
+    count = 0
     comments = []
-    for line in matrix_img:
+    for line in img_matrix:
         if '#' in line:
-            cont = 0
+            count = 0
             comments.append(line)
 
-        elif cont >= 4:
+        elif count >= 4:
             break
 
         else:
-            cont += 1
+            count += 1
 
     for comment in comments:
-        matrix_img.remove(comment)
+        img_matrix.remove(comment)
 
-    return matrix_img
+    return img_matrix
 
 # Função para obtenção de imagens em  uma matriz
-def getImageP1(name_img):
-    file_img = open(name_img, 'r')
-    matrix_img = file_img.readlines()
+def getImageP1(img_name):
+    img_file = open(img_name, 'r')
+    img_matrix = img_file.readlines()
 
-    matrix_img = clearComment(matrix_img)
+    img_matrix = clearComment(img_matrix)
 
     # Tipo de arquivo
-    type_img = matrix_img[0][0:2]
+    type_img = img_matrix[0][0:2]
 
     # Obtenção da largura e altura da imagem
-    line_size = matrix_img[1].replace('\n', '').split()
-    larg, alt = int(line_size[0]), int(line_size[1])
+    line_size = img_matrix[1].replace('\n', '').split()
+    width, height = int(line_size[0]), int(line_size[1])
 
     # Obtenção da matriz da imagem pura, sem cabeçalho e em uma lista
     ini = 2
     image = []
-    for i in range(alt):
+    for i in range(height):
         line = []
-        for j in range(ini, ini + larg):
-            line.append(int(matrix_img[j]))
+
+        for j in range(ini, ini + width):
+            line.append(int(img_matrix[j]))
+
         image.append(line)
-        ini += larg
+        ini += width
 
     image_np = np.array(image)
-    file_img.close()
+    img_file.close()
 
-    return (type_img, larg, alt, image_np)
+    return (type_img, width, height, image_np)
 
 # Função que salva uma matriz como imagem
-def saveImage(type_img, larg, alt, image):
-    name_img = "imagem" + str(random.randint(0, 10000)) + ".pgm"
+def saveImage(type_img, width, height, image):
+    img_name = "imagem" + str(random.randint(0, 10000)) + ".pgm"
     type_img = type_img + "\n"
-    size = str(larg) + " " + str(alt) + "\n"
+    size = str(width) + " " + str(height) + "\n"
     header = [type_img, size]
 
-    file_img = open('../results/'+name_img, 'w')
+    file_img = open('../results/'+img_name, 'w')
 
     # Escreve o cabeçalho
     for content in header:
         file_img.write(content)
 
-    for i in range(alt):
-        for j in range(larg):
+    for i in range(height):
+
+        for j in range(width):
             pixel = str(image[i][j]) + '\n'
             file_img.write(pixel)
 
     file_img.close()
 
 # Função que dilata a imagem
-def dilateEffect(image, larg, alt, elem_struct):
+def dilateEffect(image, width, height, elem_struct):
     # Informações do elemento estruturante
-    larg_elem = len(elem_struct[0])
-    alt_elem = len(elem_struct)
-    center = int(alt_elem / 2)
+    elem_width = len(elem_struct[0])
+    elem_height = len(elem_struct)
+    center = int(elem_height / 2)
 
+    # Cópia da imagem
     img_res = image.copy()
 
-    for i in range(1, alt-center):
+    for i in range(1, height - center):
 
-        for j in range(1, larg-center):
-            ver = False
+        for j in range(1, width - center):
+            check = False
 
-            for ie in range(alt_elem):
+            for ie in range(elem_height):
 
-                for je in range(larg_elem):
+                for je in range(elem_width):
                     n_i = (i-1) + ie
                     n_j = (j-1) + je
 
                     if (elem_struct[ie][je] != 0) and (image[n_i][n_j] == elem_struct[ie][je]):
                         img_res[i - 1 + center][j - 1 + center] = elem_struct[center][center]
-                        ver = True
+                        check = True
                         break
 
-                if ver:
+                if check:
                     break
 
     return img_res
@@ -109,28 +114,27 @@ def subtractionEffect(image1, image2):
     return img_res
 
 # Identifica as bordas inferiores dos objetos na imagem
-def identifyBorder(name_img):
-    type_img, larg, alt, image = getImageP1(name_img)
+def identifyBorder(img_name):
+    type_img, width, height, image = getImageP1(img_name)
 
     image_copy = image.copy()
     elem_struct = np.array([[0, 1, 0], [0, 1, 0], [0, 0, 0]])
 
-    image_dilated = dilateEffect(image_copy, larg, alt, elem_struct)
-    image_sub = subtractionEffect(image_dilated, image_copy)
+    dilated_image = dilateEffect(image_copy, width, height, elem_struct)
+    subtracted_image = subtractionEffect(dilated_image, image_copy)
 
-    saveImage(type_img, larg, alt, image_sub)
+    saveImage(type_img, width, height, subtracted_image)
 
-    return (type_img, larg, alt, image_sub)
+    return (type_img, width, height, subtracted_image)
       
-
 # Identifica a quantidade de polígonos presentes na imagem
-def identifyPolygon(tipo_img, larg, alt, image, img_name):
+def identifyPolygon(img_type, width, height, image, img_name):
     
     # Percorre as coordenadas salvando os pixels pretos formando as linhas
     coord = []
-    for i in range(alt):
+    for i in range(height):
 
-        for j in range(larg):
+        for j in range(width):
 
             if image[i][j] == 1:
                 coord.append([i, j])
@@ -143,49 +147,49 @@ def identifyPolygon(tipo_img, larg, alt, image, img_name):
     lines = [[]]
     num_lines = 0
 
-    # identificacao de retas continuas dos poligonos   
+    # Identificacao de retas continuas dos poligonos   
     while len(coord_copy) != 0:
         i, j = coord_copy[0]
 
-        veri = 10
-        ant_ver = 10 
+        checks = 10
+        old_check = 10 
 
-        while veri != 0:
-                
+        while checks != 0:
+               
             if ([i, j + 1] in coord_copy):
                 lines[num_lines].append([i, j])
-                coord_copy.remove([i,j])
-                j = j+1
-                veri += 1
+                coord_copy.remove([i, j])
+                j = j + 1
+                checks += 1
                     
             if ([i + 1, j + 1] in coord_copy):
                 lines[num_lines].append([i, j])
-                coord_copy.remove([i,j])
-                i = i+1
-                j = j+1
-                veri += 1
+                coord_copy.remove([i, j])
+                i = i + 1
+                j = j + 1
+                checks += 1
 
-            if ([i+1,j] in coord_copy):
+            if ([i + 1,j] in coord_copy):
                 lines[num_lines].append([i, j])
-                coord_copy.remove([i,j])
-                i = i+1
-                veri += 1
+                coord_copy.remove([i, j])
+                i = i + 1
+                checks += 1
 
-            if ([i-1, j] in coord_copy):
+            if ([i - 1, j] in coord_copy):
                 lines[num_lines].append([i, j])
-                coord_copy.remove([i,j])
-                i = i-1
-                veri += 1
+                coord_copy.remove([i, j])
+                i = i - 1
+                checks += 1
 
             if ([i - 1, j + 1] in coord_copy):
                 lines[num_lines].append([i, j])
-                coord_copy.remove([i,j])
-                i = i-1
-                j = j+1
-                veri += 1
+                coord_copy.remove([i, j])
+                i = i - 1
+                j = j + 1
+                checks += 1
 
-            veri = veri - ant_ver 
-            ant_ver = veri
+            checks = checks - old_check 
+            old_check = checks
 
         lines[num_lines].append([i, j])
         coord_copy.remove([i,j])
@@ -198,89 +202,97 @@ def identifyPolygon(tipo_img, larg, alt, image, img_name):
 
     # Lista de polígonos e buracos
     polygons = []
-    buracos = []
+    holes = []
     
     # Coordenadas
-    cord_retas = []
-    cord_buraco = []
+    coord_lines = []
+    coord_holes = []
     
     for line in lines:
         if len(line) > 10:
             polygons.append(line)
-            cor = [[], []]
+            coord = [[], []]
             
-            for ponto in line:
-                cor[0].append(648-ponto[0])
-                cor[1].append(ponto[1])
+            for pair in line:
+                coord[0].append(648 - pair[0])
+                coord[1].append(pair[1])
 
-            cord_retas.append(cor)
+            coord_lines.append(coord)
             count += 1
                
         elif len(line) > 6:
-            buracos.append(line)
+            holes.append(line)
+    
+    # Lista de remoções
+    removed = []
 
-    removidos = []
-    id_pol = 1
+    # Referência para o polígono
+    polygon_id = 1
 
-    # numero de poligonos com buracos
-    n_pol_buracos = 0
+    # Número de poligonos com buracos
+    polygons_holes_num = 0
     
     for polygon in polygons:
-        n_buracos = 0
+        holes_num = 0
         
         meio = int(len(polygon)/2)
         
-        for buraco in buracos:
-            isIn = (polygon[0][1] < buraco[0][1] < polygon[-1][1]) and (polygon[meio][0] > buraco[0][0])
-            if (isIn and not(buraco in removidos)):
+        for hole in holes:
+            isIn = (polygon[0][1] < hole[0][1] < polygon[-1][1]) and (polygon[meio][0] > hole[0][0])
+            
+            if (isIn and not(hole in removed)):
                 
-                n_buracos += 1
-                removidos.append(buraco)
+                holes_num += 1
+                removed.append(hole)
 
-                cor = [[], []]
+                coor = [[], []]
             
-                for ponto in buraco:
-                    cor[0].append(648-ponto[0])
-                    cor[1].append(ponto[1])
+                for pair in hole:
+                    coor[0].append(648 - pair[0])
+                    coor[1].append(pair[1])
 
-                    cord_buraco.append(cor)
+                    coord_holes.append(coor)
 
-        if n_buracos > 0:
-            n_pol_buracos += 1
+        if holes_num > 0:
+            polygons_holes_num += 1
             
-        print("Poligono "+ str(id_pol) + "- n° de buracos: "+ str(n_buracos))
+        print("Polígono " + str(polygon_id) + "- n° de buracos: " + str(holes_num))
 
-        id_pol += 1
+        polygon_id += 1
     
-    for y, x in cord_retas:
+    for y, x in coord_lines:
         plt.plot(x,y)
 
-    for y,x in cord_buraco:
+    for y,x in coord_holes:
         plt.plot(x,y)
 
     graph_name = img_name[10:]
-    graph_name = "./../results"+graph_name[:-4]+"_grafico"+".png"
+    graph_name = "./../results" + graph_name[:-4] + "_grafico"+".png"
     
     plt.savefig(graph_name, dpi=300)
     plt.close()
 
     print("---------------------- Geral --------------------------")
-    print("N° de poligonos com buracos: " + str(n_pol_buracos))
-    print("N° de poligonos sem buracos: " + str(count - n_pol_buracos))
-    print("Total de poligonos: " + str(count) )
-    print("Total de buracos: " + str(len(buracos)))
+    print("N° de polígonos com buracos: " + str(polygons_holes_num))
+    print("N° de polígonos sem buracos: " + str(count - polygons_holes_num))
+    print("Total de polígonos: " + str(count))
+    print("Total de buracos: " + str(len(holes)))
     print()
 
-# main
-path_images = "./../tests/"
-walk = list(os.walk(path_images))
+# Main
+start_time = time.time()
+images_path = "./../tests/"
+walk = list(os.walk(images_path))
 _,_,files = walk[0]
 
 for file in files:
     print("Imagem " + str(file))
     
-    img_name = path_images + file
+    img_name = images_path + file
     
-    tipo_img, larg, alt, image = identifyBorder(img_name)
-    identifyPolygon(tipo_img, larg, alt, image, img_name)
-  
+    img_type, width, height, image = identifyBorder(img_name)
+    identifyPolygon(img_type, width, height, image, img_name)
+ 
+final_time = time.time()
+total_time = final_time - start_time
+print("\nTempo total de execução: %.3f ms" %total_time)
